8390e4f48193f8e41664b827504b169931e1e4c2
MarkonLehel <lehel.markon@gmail.com>
2021-08-24 16:12:57 +0200

    Clean code of SecureStorageAccess

--- SaintSender.Core/Models/SecureStorageAccess.cs
+++ SaintSender.Core/Models/SecureStorageAccess.cs
@@ -9,23 +9,8 @@ namespace SaintSender.Core.Models
     {
         IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
 
-        public void SaveUser(string email, string hashedPassword)
-        {
-            if (isoStore.FileExists("UserData.txt"))
-            {
-                if (!ReadData("UserData").Contains(email))
-                {
-                    WriteData("UserData", new List<string> { email, hashedPassword }, FileMode.Append);
-                } else
-                {
-                    throw new Exception($"The e-mail address {email} is already taken.");
-                }
-            } else
-            {
-                WriteData("UserData", new List<string> { email, hashedPassword }, FileMode.CreateNew);
-            }
-        }
-
+        #region Reading data
+        //Read from secure storage
         public Dictionary<string,string> GetUserLoginData(string email)
         {
             if (isoStore.FileExists("UserData.txt"))
@@ -48,8 +33,6 @@ namespace SaintSender.Core.Models
             }
         }
 
-        //Read from secure storage
-
         public List<string> ReadData(string fileName)
         {
             using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(fileName + ".txt", FileMode.Open, isoStore))
@@ -64,10 +47,30 @@ namespace SaintSender.Core.Models
                 }
             }
         }
+        #endregion
 
-        
-
+        #region Writing data
         //Write to secure storage
+
+        public void SaveUser(string email, string hashedPassword)
+        {
+            if (isoStore.FileExists("UserData.txt"))
+            {
+                if (!ReadData("UserData").Contains(email))
+                {
+                    WriteData("UserData", new List<string> { email, hashedPassword }, FileMode.Append);
+                }
+                else
+                {
+                    throw new Exception($"The e-mail address {email} is already taken.");
+                }
+            }
+            else
+            {
+                WriteData("UserData", new List<string> { email, hashedPassword }, FileMode.CreateNew);
+            }
+        }
+
         public void WriteData(string fileToWrite, List<string> data, FileMode writeMethod = FileMode.Create)
         {
             using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(fileToWrite + ".txt", writeMethod, isoStore)) 
@@ -92,6 +95,7 @@ namespace SaintSender.Core.Models
                 }
             };
         }
+        #endregion
     }
 }
 
